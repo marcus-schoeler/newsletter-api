9c497aa33519a8c31b2f2e33053b79f97888317c
marcus-schoeler <marcus.schoeler@posteo.de>
2026-04-06 22:20:56 +0200

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
         body: JSON.stringify({
           email,
           includeListIds: [7],
-          templateId: 1,
+          templateId: 10,
           redirectionUrl: "https://195093-copy5.cargo.site/danke"
         })
       }
